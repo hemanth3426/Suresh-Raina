3cf7bce7b0e7f5a1f84ce760edb70bd96ecc2d63
hemanth3426 <147473961+hemanth3426@users.noreply.github.com>
2023-10-10 12:20:41 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,9 @@
+package Operaters;
+
+public class Boolean {
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		 System.out.println(a<35 & a<b);
+	}
+}
---
+++ TotalMarks.java
@@ -0,0 +1,19 @@
+package Operaters;
+
+public class TotalMarks {
+	public static void main(String[] args) {
+		int TotalMarks=600;
+		
+		int maths=85;
+		int IOT=80;
+		int NoSql=85;
+		int English=90;
+		int Hindi=97;
+		int Management=70;
+		int Total=maths+IOT+NoSql+English+Hindi+Management;
+		double percentage=Total*100/TotalMarks;
+		System.out.println(Total);
+		System.out.println("Percentage:"+percentage+"%");
+		
+	}
+}
