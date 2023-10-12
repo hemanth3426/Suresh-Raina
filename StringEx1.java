e2d927965a48cd191bf0ddbdf2d1ce44eb40141d
hemanth3426 <147473961+hemanth3426@users.noreply.github.com>
2023-10-12 11:05:29 +0530

    Add files via upload

---
+++ StringEx1.java
@@ -0,0 +1,21 @@
+package Strings;
+//java.lang is default package in java
+public class StringEx1 {
+	public static void main(String[] args) {
+		String str="Hemanth ";
+		String str3="GOWDA";
+		
+		System.out.println(str.concat(str3));
+		
+        System.out.println(str.length());
+		
+		System.out.println(str.charAt(4));
+		System.out.println(str3.charAt(2));
+		
+		System.out.println(str.toUpperCase());
+		
+		System.out.println(str3.toLowerCase());
+	
+	}
+
+}
